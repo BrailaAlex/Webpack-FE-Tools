094c5d6bd7c9e3a8fdd1888ae16a3fa4dcb793de
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-16 14:52:43 +0300

    tools

---
+++ Lesson1/task1/src/scripts/logger/logger.js
@@ -0,0 +1,16 @@
+//logger function
+
+export const createLogger = name => {
+    const logs = [];
+    return {
+        log(message) {
+            logs.push(`log - ${name} - ${message}`);
+        },
+        error(errorText) {
+            logs.push(`error - ${name} - ${errorText}`);
+        },
+        getLogs() {
+            return logs;
+        }
+    }
+}
